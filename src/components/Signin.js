ca5496565b657db924dadf6f30cd01a11e7bd344
Samir Kaushik <samirkaushik2002@gmail.com>
2022-12-10 12:15:05 +0530

    added proper error toasts

--- src/components/Signin.js
+++ src/components/Signin.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useRef, useState } from "react";
-import {signInWithEmailAndPassword,createUserWithEmailAndPassword} from "firebase/auth"
-import { auth,db } from "./firebase";
+import { signInWithEmailAndPassword, createUserWithEmailAndPassword } from "firebase/auth"
+import { auth, db } from "./firebase";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
@@ -12,14 +12,21 @@ const Signin = () => {
 
     const signIn = (e) => {
         e.preventDefault();
-        signInWithEmailAndPassword( auth,emailContainer.current.value, passwordContainer.current.value)
-          .then((userCredential) => { 
-            const user = userCredential.user;
-          })
-          .catch((error) => {
-            const errorMessage = error.message;
-            toast.error(errorMessage);
-          });
+        signInWithEmailAndPassword(auth, emailContainer.current.value, passwordContainer.current.value)
+            .then((userCredential) => {
+                const user = userCredential.user;
+            })
+            .catch((error) => {
+                if (error.code === 'auth/wrong-password') {
+                    toast.error('Please check the Password');
+                }
+                if (error.code === 'auth/user-not-found') {
+                    toast.error('Please check the Email');
+                }
+                if (error.code === 'auth/invalid-email') {
+                    toast.error('Invalid email');
+                }
+            });
     }
 
     const signUp = (e) => {
@@ -27,22 +34,29 @@ const Signin = () => {
         setRegister(true);
         createUserWithEmailAndPassword(auth, emailContainer.current.value, passwordContainer.current.value)
             .then((userCredential) => {
-            
+
                 const user = userCredential.user;
                 setRegister(false)
                 console.log(userCredential)
 
             })
             .catch((error) => {
-                const errorMessage = error.message;
-                toast.error(errorMessage);
+                if (error.code === 'auth/email-already-in-use') {
+                    toast.error('Email Already in Use');
+                }
+                if (error.code === 'auth/invalid-email') {
+                    toast.error('Invalid email');
+                }
+                if (error.code === 'auth/invalid-password') {
+                    toast.error('Password must be atleast 6 digits long');
+                }
             })
     }
 
     return (
         <>
             <form action="" className="container">
-            {register ? <h1>Sign Up</h1> : <h1>Sign In</h1>}
+                {register ? <h1>Sign Up</h1> : <h1>Sign In</h1>}
                 <label htmlFor="email">Email: </label>
                 <input
                     type="email"
@@ -57,10 +71,10 @@ const Signin = () => {
                     ref={passwordContainer}
                 />
 
-                <button type="submit" onClick={register?signUp:signIn}>Submit</button>
-                {register || <div onClick={signUp} style={{cursor:'pointer',fontWeight:"bolder"}}>Sign Up for a new account</div>}
+                <button type="submit" onClick={register ? signUp : signIn}>Submit</button>
+                {register || <div onClick={signUp} style={{ cursor: 'pointer', fontWeight: "bolder" }}>Sign Up for a new account</div>}
             </form>
-            <ToastContainer/>
+            <ToastContainer />
         </>
     );
 }
